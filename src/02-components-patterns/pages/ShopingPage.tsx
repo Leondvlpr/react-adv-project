e8c057f82c7c3ec8ab37568b1d564638d147a8b3
Esteban Leon <esteban.leon@zoomcem.com>
2022-10-27 01:17:02 -0500

    Component compound listo

---
+++ src/02-components-patterns/pages/ShopingPage.tsx
@@ -0,0 +1,29 @@
+import { ProductButtons, ProductCard, ProductImage, ProductTitle } from "../components";
+
+const product = {
+    id: '1',
+    title: 'Coffe Mug - Card',
+    img: './coffee-mug.png'
+}
+
+export const ShopingPage = () => {
+    return (
+        <div>
+            <h1>Shopping Store</h1>
+            <hr />
+            <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+                <ProductCard product={product}>
+                    <ProductCard.Image />
+                    <ProductCard.Title title={'Café'} />
+                    <ProductCard.Buttons />
+                </ProductCard>
+
+                <ProductCard product={product}>
+                    <ProductImage />
+                    <ProductTitle />
+                    <ProductButtons />
+                </ProductCard>
+            </div>
+        </div>
+    )
+}
